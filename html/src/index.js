dd44a32c7ab6487d5fb8c5c98e46b128216e2b9a
Karol Nosek <karnn022@gmail.com>
2024-05-24 12:18:01 +0200

    error changes

--- html/src/index.js
+++ html/src/index.js
@@ -5,16 +5,7 @@ import {getAuth,
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword} 
     from 'firebase/auth';
-import { 
-    email_field,
-    password_field,
-    log_btn,
-    showLoginError,
-    email_reg_field,
-    password_reg_field,
-    password_repeat,
-    sign_btn
-} from './ui.js';
+import * as ui from './ui.js';
 
 
 
@@ -34,12 +25,12 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 // firebase authorization instance
 const auth = getAuth(firebaseApp);
-connectAuthEmulator(auth, "http://localhost:9099");
+//connectAuthEmulator(auth, "http://localhost:9099");
 
 // callback for login button
 const login_email_pass = async() =>{
-    const login_email = email_field.value;
-    const login_password = password_field.value; 
+    const login_email = ui.email_field.value;
+    const login_password = ui.password_field.value; 
     try{
         const userCredentials = await signInWithEmailAndPassword(auth, login_email, login_password);
         console.log(userCredentials.user);
@@ -47,23 +38,24 @@ const login_email_pass = async() =>{
     }
     catch(error){
         console.log("bład lol");
-        showLoginError(error);
+        ui.showLoginError(error);
     }    
 
 }
 // callback for signup button
 const acc_creation = async() =>{
-    const registration_email = email_reg_field.value;
-    const registration_password = password_reg_field.value;
-    const password_confiramtion = password_repeat.value;
+    const registration_email = ui.email_reg_field.value;
+    const registration_password = ui.password_reg_field.value;
+    const password_confiramtion = ui.password_repeat.value;
     if(registration_password == password_confiramtion){
         try{
             await createUserWithEmailAndPassword(auth, registration_email, registration_password);
+            ui.sukces('gg')
             console.log("gj")
         }
         catch(error){
             console.log("error lol");
-            showLoginError(error);
+            ui.showLoginError(error);
         }    
     }
     else{
@@ -72,13 +64,13 @@ const acc_creation = async() =>{
 }
 
 // Waiting for a click on the login button
-if (log_btn){
-    log_btn.addEventListener("click", login_email_pass);
+if (ui.log_btn){
+    ui.log_btn.addEventListener("click", login_email_pass);
 }
 
 // Waiting for a click on the signup button 
-if (sign_btn){
-    sign_btn.addEventListener("click", acc_creation);
+if (ui.sign_btn){
+    ui.sign_btn.addEventListener("click", acc_creation);
 }
 
 
--- html/src/ui.js
+++ html/src/ui.js
@@ -12,12 +12,19 @@ export const sign_btn = document.querySelector('#reg_button') // sign-up button 
 // Login error
 export const showLoginError = (error) => { 
     if (error.code == AuthErrorCodes.INVALID_EMAIL) {
-        login_error.innerHTML = `Wrong email. Try again.`
+      login_error.innerHTML = `Please provide a proper email adress.`
       }
     else if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
-        login_error.innerHTML = `Wrong password. Try again.`
+      login_error.innerHTML = `Please provide password that is at least 6 characters long.`
+    }
+    else if (error.code == AuthErrorCodes.INVALID_LOGIN_CREDENTIALS){
+      login_error.innerHTML = `Wrong email or password.`
     }
     else {
-        login_error.innerHTML = `Error: ${error.message}`      
+      login_error.innerHTML = `Error: ${error.message}`      
     }
+  }
+
+  export const sukces = (clear) => {
+    login_error.innerHTML = 'Konto zostało utworzone, przejdź do strony logownaia.'
   }
