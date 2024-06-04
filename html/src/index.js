37b86bb074fea60872d455a654f34ed38b8c363d
Karol Nosek <karnn022@gmail.com>
2024-06-04 16:44:31 +0200

    Added somewhat functional chat feature

--- html/src/index.js
+++ html/src/index.js
@@ -1,4 +1,6 @@
 import {initializeApp} from 'firebase/app';
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { collection, addDoc, onSnapshot, query, orderBy } from "firebase/firestore"
 
 import {
     getAuth, 
@@ -6,14 +8,11 @@ import {
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
     onAuthStateChanged,
-    signOut
+    signOut,
+    updateProfile 
 } 
     from 'firebase/auth';
 import * as ui from './ui.js';
-
-var userCredentials = null;
-
-
 
 // firebase project config
 const firebaseConfig = {
@@ -31,7 +30,12 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 // firebase authorization instance
 const auth = getAuth(firebaseApp);
-connectAuthEmulator(auth, "http://localhost:9099");
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(firebaseApp);
+
+/* Authorization and Firestore emulator, ucoment when using emulators */
+// connectFirestoreEmulator(db, '127.0.0.1', 8080);
+// connectAuthEmulator(auth, "http://localhost:9099");
 
 // callback for login button
 const login_email_pass = async() =>{
@@ -39,10 +43,10 @@ const login_email_pass = async() =>{
     const login_password = ui.password_field.value; 
     try{
         const userCredentials = await signInWithEmailAndPassword(auth, login_email, login_password);
+        visitor = userCredentials;
         console.log(userCredentials.user);
         console.log(login_error, "gj");
         window.location.href = "glowna.html"  
-        // monitor_auth_state();
     }
     catch(error){
         console.log("bład lol");
@@ -50,6 +54,7 @@ const login_email_pass = async() =>{
     }    
 
 }
+
 // callback for signup button
 const acc_creation = async() =>{
     const registration_email = ui.email_reg_field.value;
@@ -59,7 +64,15 @@ const acc_creation = async() =>{
         try{
             await createUserWithEmailAndPassword(auth, registration_email, registration_password);
             console.log("gj")
-            window.location.href = "login.html"             
+            window.location.href = "login.html"     
+            
+            updateProfile(auth.currentUser, {
+                displayName: registration_email
+              }).then(() => {
+                
+              }).catch((error) => {
+                console.log("error lol");
+              });
         }
         catch(error){
             console.log("error lol");
@@ -72,49 +85,105 @@ const acc_creation = async() =>{
     }
 }
 
+// monitoring if a user is logged in
 const monitor_auth_state = async () =>{
     onAuthStateChanged(auth, user => {
         if (user) {
-          console.log(user)
-        if (window.location.pathname.endsWith('glowna.html') == false) {
-            window.location.href = "glowna.html";
+            console.log(user)
+            if (window.location.pathname.endsWith('glowna.html') == false && window.location.pathname.endsWith('chat.html') == false) {
+                window.location.href = "glowna.html";  
+            } 
+            if(window.location.pathname.endsWith('chat.html') == true){
+                const chosen_chatter = localStorage.getItem('value1');
+                loadMessages(chosen_chatter);
+            }
         } 
-      
-          // ...
-        } else {
-
+        else if (!user) {
+            if (window.location.pathname.endsWith('glowna.html') || window.location.pathname.endsWith('chat.html')) {
+                window.location.href = "login.html";
+            } 
         }
       });
 }
+ 
+// function to get a name of a person to chat with
+function chatting() {
+    const logged_user = auth.currentUser.displayName.substring(0,auth.currentUser.displayName.indexOf("@"))
+    const goto_user = ui.chatter_name.value.substring(0,ui.chatter_name.value.indexOf("@"))
+    const outcome = logged_user.localeCompare(goto_user);
+    let chosen_chatter;
+    if (outcome <= 0){
+        chosen_chatter = logged_user + goto_user;
+    } 
+    else {
+        chosen_chatter = goto_user + logged_user;
+    }  
+    localStorage.setItem('value1', `${chosen_chatter}`);
+    window.location.href = 'chat.html'
 
-  if (window.location.pathname.endsWith('login.html')){
-      
-  }
-  
-  
-  const logout = async () =>{
+}
+
+// loading sent messages onto the page
+function loadMessages(user) {
+    const messagesQuery = query(collection(db, user+'_messages'), orderBy('timestamp'));
+    console.log(user)
+    onSnapshot(messagesQuery, snapshot => {
+        ui.chat_window.innerHTML = '';
+        snapshot.forEach(doc => {
+            const message = doc.data();
+            const messageElement = document.createElement('div');
+            messageElement.textContent = `${message.name}: ${message.text}`;
+            ui.chat_window.appendChild(messageElement);
+        });
+    });
+}
+
+// logout
+const logout = async () =>{
     await signOut(auth).then(() =>{
     window.location.href = 'login.html'
-    })
-    
-  
-    
-  }
-  
+    }) 
+}  
 
-// Waiting for a click on the login button
+// buttons functionality
+// click on the login button
 if (ui.log_btn){
     ui.log_btn.addEventListener("click", login_email_pass);
 }
 
-// Waiting for a click on the signup button 
+// click on the signup button 
 if (ui.sign_btn){
     ui.sign_btn.addEventListener("click", acc_creation);
 }
-
+// click on the logiut button
 if (ui.logout_btn){
     ui.logout_btn.addEventListener("click", logout);    
 }
+// accepting chosen user to chat with
+if (ui.choose_chat_btn){
+    ui.choose_chat_btn.addEventListener("click", chatting);    
+}
+// sending a message via chat
+if (ui.send_message_btn){
+    ui.send_message_btn.addEventListener('click', () => {
+        const message = ui.message_text.value;
+        const user = localStorage.getItem('value1');
+    
+        if (message && user) {
+            addDoc(collection(db, user+'_messages'), {
+                text: message,
+                name: user,
+                timestamp: new Date()
+            }).then(() => {
+                ui.message_text.value = '';
+            }).catch(error => {
+                console.error('Error sending message:', error);
+            });
+        }
+    }); 
+}
 
-
+// monitoring logged user
 monitor_auth_state();
+
+
--- html/src/ui.js
+++ html/src/ui.js
@@ -5,11 +5,16 @@ export const password_field = document.querySelector('#password') // login passw
 export const email_reg_field = document.querySelector('#emailreg') // registration email reference
 export const password_reg_field = document.querySelector('#passwordreg') // regitration password referece
 export const password_repeat = document.querySelector('#repeat') // password confirmation reference
+export const chatter_name = document.querySelector('#chatter_name') // choser user for chatting reference
+export const message_text = document.querySelector('#message_text') // field with content of the chat messaged
+export const chat_window = document.querySelector('#chat_window') // reference to chat window
 
 
 export const log_btn = document.querySelector('#btn_login') // login button reference
 export const sign_btn = document.querySelector('#reg_button') // sign-up button reference
-export const logout_btn = document.querySelector('#btn_logout') // password confirmation reference
+export const logout_btn = document.querySelector('#btn_logout') // log-out confirmation reference
+export const choose_chat_btn = document.querySelector('#choose_chat_btn') // button for accepting chosen user for chatting reference
+export const send_message_btn = document.querySelector('#message_btn') // confirmation of sendiing messege via chat  reference
 
 // Login error
 export const showLoginError = (div, error) => { 
